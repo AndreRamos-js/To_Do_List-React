a7db4ec1352a4f155126f5d8ffb3496e433ca251
André Ramos <83097746+AndreRamos-py@users.noreply.github.com>
2023-11-17 23:31:48 -0300

    funcoes para adicionar e editar tarefas

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,5 +1,7 @@
 import { db } from '../db.js';
 
+
+
 export const getUsers = (_, res) => {
     const q = "SELECT * FROM tarefas";
 
@@ -8,4 +10,42 @@ export const getUsers = (_, res) => {
 
         return res.status(200).json(data);
     });
+};
+
+
+export const addUser = (req, res) => {
+    const q = 
+        'INSERT INTO tarefas(`titulo`, `descricao`, `status`, `tempo_estimado`) VALUES(?)';
+
+    const values = [
+        req.body.titulo,
+        req.body.descricao,
+        req.body.status,
+        req.body.tempo_estimado,
+    ]
+
+    db.query(q, [values], (err) => {
+        if (err) return res.json(err);
+
+        return res.status(200).json('Tarefa adicionada com sucesso!')
+    })
+};
+
+
+export const updateUser = (req, res) => {
+    const q = 
+        'UPDATE tarefas(`titulo`, `descricao`, `status`, `tempo_estimado`) VALUES(?)';
+
+    const values = [
+        req.body.titulo,
+        req.body.descricao,
+        req.body.status,
+        req.body.tempo_estimado,
+    ]
+
+    db.query(q, [values], (err) => {
+        if (err) return res.json(err);
+
+        return res.status(200).json('Tarefa adicionada com sucesso!')
+    })
 };
