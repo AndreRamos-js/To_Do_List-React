1cecda62017dcd06dbfcb44bd9deee7fc8cd33f3
André Ramos <83097746+AndreRamos-py@users.noreply.github.com>
2023-11-21 16:27:00 -0300

    fix: api

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import tarefasRoutes from "./routes/tarefas.js";
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 8800;
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use("/", tarefasRoutes);
 
