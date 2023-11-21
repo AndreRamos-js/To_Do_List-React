e0fe650a8ec552b60f252bf2c2093cbe4527df56
André Ramos <83097746+AndreRamos-py@users.noreply.github.com>
2023-11-21 15:31:22 -0300

    .

--- api/index.js
+++ api/index.js
@@ -1,22 +1,14 @@
 import express from 'express';
 import cors from 'cors';
-import tarefasRoutes from './routes/tarefas.js';
+import tarefasRoutes from "./routes/tarefas.js";
 
 const app = express();
 const port = process.env.PORT || 8800;
 
-// Configuração do middleware CORS
-const corsOptions = {
-  origin: 'https://to-do-list-9wl3.vercel.app/',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-  optionsSuccessStatus: 204,
-};
 
-app.use(cors(corsOptions));
 app.use(express.json());
-app.use('/', tarefasRoutes);
-
+app.use(cors());
+app.use("/", tarefasRoutes);
 app.listen(port, () => {
-  console.log(`Servidor está rodando na porta ${port}`);
+    console.log(`Servidor está rodando na porta ${port}`);
 });
